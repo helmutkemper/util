73ff5f85fee3b539c82a89d1635ba54cee474114
helmutkemper <helmut.kemper@ahgora.com.br>
2019-11-21 10:17:09 -0300

    math round

--- util_test.go
+++ util_test.go
@@ -3,10 +3,28 @@ package util
 import "fmt"
 
 func ExampleRemoveSpellingAccents() {
-  var text string = "áéíóúàèìòùâêîôûãẽĩõũäëïöüñçÁÉÍÓÚÀÈÌÒÙÂÊÎÔÛÃẼĨÕŨÄËÏÖÜÑÇ"
+	var text string = "áéíóúàèìòùâêîôûãẽĩõũäëïöüñçÁÉÍÓÚÀÈÌÒÙÂÊÎÔÛÃẼĨÕŨÄËÏÖÜÑÇ"
 
-  fmt.Print( RemoveSpellingAccents( text ) )
+	fmt.Print(RemoveSpellingAccents(text))
 
-  // Output:
-  // aeiouaeiouaeiouaeiouaeiouncAEIOUAEIOUAEIOUAEIOUAEIOUNC
+	// Output:
+	// aeiouaeiouaeiouaeiouaeiouncAEIOUAEIOUAEIOUAEIOUAEIOUNC
+}
+
+func ExampleRound() {
+
+	fmt.Printf("%v\n", Round(1.22))
+	fmt.Printf("%v\n", Round(1.22, 0.2))
+	fmt.Printf("%v\n", Round(1.22, 0.5))
+	fmt.Printf("%v\n", Round(1.22, 0.7))
+	fmt.Printf("%v\n", Round(1.22, 0.5, 1.0))
+	fmt.Printf("%v\n", Round(1.33, 0.5, 1.0))
+
+	// Output:
+	// 1
+	// 2
+	// 1
+	// 1
+	// 1.2
+	// 1.3
 }
