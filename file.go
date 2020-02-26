222fd46c5d650c3fe91ae595d0564198bca871ae
Helmut Kemper <helmut.kemper@gmail.com>
2020-02-26 18:32:40 -0300

    trabalhando

--- file.go
+++ file.go
@@ -2,23 +2,21 @@ package util
 
 import "os"
 
-/*
-func check(){
-  if _, err := os.Stat("/path/to/whatever"); os.IsNotExist(err) {
-    // path/to/whatever does not exist
-  }
-
-  if _, err := os.Stat("/path/to/whatever"); err == nil {
-    // path/to/whatever exists
-  }
-}
-*/
-
+// Deprecated: Please, use CheckFileOrDirExists(path string)
 func CheckFileExists(name string) bool {
-  if _, err := os.Stat(name); err != nil {
-    if os.IsNotExist(err) {
-      return false
-    }
-  }
-  return true
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func CheckFileOrDirExists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
 }
