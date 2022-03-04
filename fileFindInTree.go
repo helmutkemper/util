480f7eb266704fbadbc9e3fb5be42301e134f337
Helmut Kemper <helmut.kemper@gmail.com>
2022-03-04 14:24:25 -0300

    added go.mod

--- fileFindInTree.go
+++ fileFindInTree.go
@@ -6,32 +6,32 @@ import (
 	"path/filepath"
 )
 
-func FileFindInThree(name string) (filePath string, err error) {
+func FileFindInTree(name string) (filePath string, err error) {
 	var fileInfo os.FileInfo
 	err = filepath.Walk(".",
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
-
+			
 			if name == info.Name() {
 				filePath = path
 				fileInfo = info
 				return nil
 			}
-
+			
 			return nil
 		},
 	)
-
+	
 	if err != nil {
 		return
 	}
-
+	
 	if filePath == "" || fileInfo.IsDir() == true {
 		filePath = ""
 		err = errors.New("file not found")
 	}
-
+	
 	return
 }
